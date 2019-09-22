411962ae670db375066521bc87751471a7c3a9f5
swamy321 <adsswamy7@gmail.com>
2019-09-22 14:20:32 +0530

    Initial commit adding user register model

--- RBS/roomapp/admin.py
+++ RBS/roomapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from roomapp.models import accounts
+from roomapp.models import accounts,company,book_room
 # Register your models here.
 class accounts_admin(admin.ModelAdmin):
     list_display = ['email','username','Date_Joiend','last_login','password','confiem_password']
 admin.site.register(accounts,accounts_admin)
+admin.site.register(company)
+admin.site.register(book_room)
